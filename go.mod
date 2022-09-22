419cb916078cc81a6e8c4e826c798decb3258c6b
thisisaaronland <thisisaaronland@localhost>
2022-09-22 14:03:14 -0700

    snapshot: add point in polygon table

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/paulmach/orb v0.7.1
 	github.com/tidwall/gjson v1.14.3
 	github.com/whosonfirst/go-whosonfirst-database-sql v0.0.1
 	github.com/whosonfirst/go-whosonfirst-feature v0.0.24
+	github.com/whosonfirst/go-whosonfirst-uri v1.2.0
 	github.com/whosonfirst/go-writer/v3 v3.0.0
 )
 
@@ -14,9 +16,9 @@ require (
 	github.com/aaronland/go-roster v1.0.0 // indirect
 	github.com/g8rswimmer/error-chain v1.0.0 // indirect
 	github.com/natefinch/atomic v1.0.1 // indirect
-	github.com/paulmach/orb v0.7.1 // indirect
 	github.com/sfomuseum/go-edtf v1.1.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/whosonfirst/go-whosonfirst-flags v0.4.4 // indirect
+	github.com/whosonfirst/go-whosonfirst-sources v0.1.0 // indirect
 )
---
+++ tables/pointinpolygon.go
@@ -0,0 +1,132 @@
+package tables
+
+import (
+	"context"
+	"database/sql"
+	_ "embed"
+	"fmt"
+	"github.com/paulmach/orb/encoding/wkt"
+	wof_sql "github.com/whosonfirst/go-whosonfirst-database-sql"
+	"github.com/whosonfirst/go-whosonfirst-feature/geometry"
+	"github.com/whosonfirst/go-whosonfirst-feature/properties"
+	"github.com/whosonfirst/go-whosonfirst-uri"
+)
+
+//go:embed pointinpolygon.schema
+var pointinpolygon_schema string
+
+const POINTINPOLYGON_TABLE string = "pointinpolygon"
+
+type PointInPolygonTable struct {
+	wof_sql.Table
+}
+
+func NewPointInPolygonTableWithDatabase(ctx context.Context, db wof_sql.Database) (wof_sql.Table, error) {
+
+	t, err := NewPointInPolygonTable(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create new pointinpolygon table, %w", err)
+	}
+
+	err = t.InitializeTable(ctx, db)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to initialize pointinpolygon table, %w", err)
+	}
+
+	return t, nil
+}
+
+func NewPointInPolygonTable(ctx context.Context) (wof_sql.Table, error) {
+	t := PointInPolygonTable{}
+	return &t, nil
+}
+
+func (t *PointInPolygonTable) Name() string {
+	return POINTINPOLYGON_TABLE
+}
+
+// https://dev.sql.com/doc/refman/8.0/en/json-functions.html
+// https://www.percona.com/blog/2016/03/07/json-document-fast-lookup-with-mysql-5-7/
+// https://archive.fosdem.org/2016/schedule/event/mysql57_json/attachments/slides/1291/export/events/attachments/mysql57_json/slides/1291/MySQL_57_JSON.pdf
+
+func (t *PointInPolygonTable) Schema() string {
+	return pointinpolygon_schema
+}
+
+func (t *PointInPolygonTable) InitializeTable(ctx context.Context, db wof_sql.Database) error {
+	return wof_sql.CreateTableIfNecessary(ctx, db, t)
+}
+
+func (t *PointInPolygonTable) IndexRecord(ctx context.Context, db wof_sql.Database, i interface{}, custom ...interface{}) error {
+
+	conn, err := db.Conn()
+
+	if err != nil {
+		return fmt.Errorf("Failed to establish database connection, %w", err)
+	}
+
+	tx, err := conn.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+
+	if err != nil {
+		return fmt.Errorf("Failed to create transaction, %w", err)
+	}
+
+	err = t.IndexFeature(ctx, tx, i.([]byte), custom...)
+
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("Failed to index %s table, %w", t.Name(), err)
+	}
+
+	err = tx.Commit()
+
+	if err != nil {
+		return fmt.Errorf("Failed to commit transaction, %w", err)
+	}
+
+	return nil
+}
+
+func (t *PointInPolygonTable) IndexFeature(ctx context.Context, tx *sql.Tx, body []byte, custom ...interface{}) error {
+
+	id, err := properties.Id(body)
+
+	if err != nil {
+		return fmt.Errorf("Failed to derive ID, %w", err)
+	}
+
+	var alt *uri.AltGeom
+
+	if len(custom) >= 1 {
+		alt = custom[0].(*uri.AltGeom)
+	}
+
+	if alt != nil {
+		return nil
+	}
+
+	geojson_geom, err := geometry.Geometry(body)
+
+	if err != nil {
+		return fmt.Errorf("Failed to derive geometry, %w", err)
+	}
+
+	orb_geom := geojson_geom.Geometry()
+	wkt_geom := wkt.MarshalString(orb_geom)
+
+	q := fmt.Sprintf(`REPLACE INTO %s (
+		geometry, id
+	) VALUES (
+		ST_GeomFromText('%s'), ?
+	)`, POINTINPOLYGON_TABLE, wkt_geom)
+
+	_, err = tx.ExecContext(ctx, q, id)
+
+	if err != nil {
+		return fmt.Errorf("Failed to update table, %w", err)
+	}
+
+	return nil
+}
